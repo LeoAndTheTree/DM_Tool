31b178a64ef488f1645e0eef43f436790c842bf4
Yunsong Liu <yunsongliu@Yunsongs-MacBook-Pro.local>
2020-05-20 11:16:35 -0400

    working canvas with movement

--- Map.py
+++ Map.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image as im
+from PIL import ImageTk as imtk
 
 # new stuff:
 # add definition for wall, air, door, trap
@@ -26,13 +27,13 @@ class Cell:
     _locn = None
     _tangible = None
     _visible = None
-    _image = None
+    _imageDir = None
 
     def __init__(self, row, col, imageDir):
         self._locn = Locn(row, col)
-        self._image = im.open(imageDir)
+        self._imageDir = imageDir
 
-    def __hash__(self): return 13 * hash(_tangible) + 97 * hash(_visible) + hash(_locn)
+    def __hash__(self): return 13 * hash(self._tangible) + 97 * hash(self._visible) + hash(self._locn)
 
     def __eq__(self, other):
         return (other != None and
@@ -45,6 +46,19 @@ class Cell:
 
     def getLocn(self): return self._locn
 
+    def _isVisible(self): return self._visible
+
+class Air(Cell):
+    def __init__(self, row, col, imageDir):
+        Cell.__init__(self, row, col, imageDir)
+        self._visible = False
+        self._tangible = False
+
+class Wall(Cell):
+    def __init__(self, row, col, imageDir):
+        Cell.__init__(self, row, col, imageDir)
+        self._visible = True
+        self._tangible = False
 
 class Map:
     _grid = None # 2D array
@@ -63,8 +77,68 @@ class Map:
         self._grid = np.empty(shape=(rowMax + 1, colMax + 1), dtype=np.object)
         for cell in cells:
             self._grid[cell._locn.row, cell._locn.col] = cell
-    
+
+    def myRange(self, r):
+        n = int(r + 0.5)
+        if(n >= 0):
+            return range(n)[1:n]
+        elif (n < 0):
+            return map(lambda x : x * -1, range(n)[1:n])
+
+    # changing the definition to the center of target, so expect 
     # we need to add calculation of reachable cells (BFS)
     # also line of sight
+    def intersect(self, x1, y1, x2, y2):
+        # expect x2 and y2 are 0.5, 0.5 (center of cell)
+        needToCheck = set([])
+        if(x2 == x1): return True # this will realistically never happen
+        k = float(y2 - y1)/(x2 - x1)
+        for i in self.myRange(x2 - x1):
+            newx = x1 + i
+            newy = y1 + i * k
+            if (int(newy) == newy): # integer grid point
+                needToCheck.add(self._grid[int(newy)][newx])
+                needToCheck.add(self._grid[int(newy - 1)][newx])
+                needToCheck.add(self._grid[int(newy)][newx - 1])
+                needToCheck.add(self._grid[int(newy - 1)][newx - 1])
+            else: # non interger grid point
+                needToCheck.add(self._grid[int(newy)][newx])
+                needToCheck.add(self._grid[int(newy)][newx - 1])
+        
+        for i in self.myRange(y2 - y1):
+            newx = x1 + i / k
+            newy = y1 + i
+            if (int(newy) == newy): # integer grid point
+                needToCheck.add(self._grid[newy][int(newx)])
+                needToCheck.add(self._grid[newy - 1][int(newx)])
+                needToCheck.add(self._grid[newy][int(newx - 1)])
+                needToCheck.add(self._grid[newy - 1][int(newx - 1)])
+            else: # non interger grid point
+                needToCheck.add(self._grid[newy][int(newx)])
+                needToCheck.add(self._grid[newy - 1][int(newx)])
+        for cell in needToCheck:
+            if(cell._visible):
+                return False
+        return True
+
     def _lineOfSight(self, cell1, cell2):
-        return
+        # we will find each corner and do intersect
+        r1 = cell1._locn.row
+        c1 = cell1._locn.col
+        r2 = cell2._locn.row + 0.5
+        c2 = cell2._locn.col + 0.5
+        return self.intersect(r1, c1, r2, c1) or self.intersect(r1+1, c1, r2, c1) \
+            or self.intersect(r1+1, c1+1, r2, c1) or self.intersect(r1, c1+1, r2, c1)
+
+"""
+cells = []
+for i in range(6):
+    for j in range(6):
+        if(i == 1 and j == 1):
+            cells.append(Wall(i, j, "blank"))
+        else:
+            cells.append(Air(i, j, "Assets/stone2.jpg"))
+
+map = Map(cells)
+print(map._lineOfSight(map._grid[0, 1], map._grid[1, 0]))
+"""
--- window.py
+++ window.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tkinter as tk
 from Map import *
+from PIL import Image as im
+import PIL.ImageTk as imtk
 
 # what I want to add after this:
 # resizing canvas, drawing pictures inside canvas
@@ -10,11 +12,15 @@ from Map import *
 class drawCell(tk.Canvas):
     width = 0
     height = 0
+    myscale = 1.0
+    scaleFactor = 1.1
     def __init__ (self, master=None, **kwargs):
         tk.Canvas.__init__(self, master, **kwargs)
         self.grid()
         # self.bind("<Configure>", self.on_resize)
         self.bind("<MouseWheel>", self._on_mousewheel)
+        self.bind("<B1-Motion>", self._on_mousemotion)
+        self.bind("<ButtonPress-1>", self._on_mousePress)
         self.width = self.winfo_reqwidth()
         self.height = self.winfo_reqheight()
         # self.drawBorder()
@@ -29,13 +35,36 @@ class drawCell(tk.Canvas):
         self.config(width=self.width, height=self.height)
         # rescale all the objects tagged with the "all" tag
         self.scale('all',0,0,wscale,hscale)
+        self.config(scrollregion=self.bbox('all'))
     
+    def _on_mousePress(self, event):
+        self.oldx = event.x
+        self.oldy = event.y
+
+    def _on_mousemotion(self, event):
+        self.xview_scroll(int ((self.oldx - event.x)), "units")
+        self.yview_scroll(int ((self.oldy - event.y)), "units")
+        self.oldx = event.x
+        self.oldy = event.y
+
     def _on_mousewheel(self, event):
         # print(event.state)
+        if (event.delta > 0):
+            self.config(width=self.winfo_width()*self.scaleFactor, height=self.winfo_height()*self.scaleFactor)
+            self.myscale*=self.scaleFactor
+            self.scale('all', event.x, event.y, self.scaleFactor, self.scaleFactor)
+        elif (event.delta < 0):
+            self.config(width=self.winfo_width()/self.scaleFactor, height=self.winfo_height()/self.scaleFactor)
+            self.myscale/=self.scaleFactor
+            self.scale('all', event.x, event.y, 1/self.scaleFactor, 1/self.scaleFactor)
+        self.config(scrollregion=self.bbox('all'))
+        """
         if (event.state == 0): # vertical scrolling
             self.yview_scroll(-1*(event.delta), "units")
         if (event.state == 1): # horizontal scrolling
             self.xview_scroll(-1*(event.delta), "units")
+        """
+    
     
     def drawBorder(self):
         self.create_line(0, 0, 0, self.width - 1, self.width - 1, self.height - 1, self.height - 1, 0, 0, 0)
@@ -43,8 +72,9 @@ class drawCell(tk.Canvas):
 class Application(tk.Frame):
     _map = None # this is the representation of the map
     _canvas = None # just the canvas
+    _listOfImages = []
     def __init__ (self, master=None, cells=None):
-        tk.Frame.__init__(self, master)
+        tk.Frame.__init__(self, master,width=600, height=600)
         self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
         self.createWidget(cells)
 
@@ -58,9 +88,8 @@ class Application(tk.Frame):
         self.columnconfigure(0, weight=1)
         # creating maps and canvas for each cell
         self._map = Map(cells)
-        self._canvas = drawCell(self,highlightthickness=0)
+        self._canvas = drawCell(self,highlightthickness=0, xscrollincrement='1p', yscrollincrement='1p')
         self._canvas.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-        
         width = self._canvas.width
         height = self._canvas.height
         row = self._map._row
@@ -68,13 +97,18 @@ class Application(tk.Frame):
         for i in range(row):
             for j in range(col):
                 self._canvas.create_line(i*100, j*100, i*100+100, j*100, i*100+100, j*100+100, i*100, j*100+100, i*100, j*100)
-                self._canvas.create_image(self._map._grid[i][j]._image)
+                """
+                image = im.open(self._map._grid[i][j]._imageDir)
+                tkImage = imtk.PhotoImage(image.resize((100, 100)))
+                self._listOfImages.append(tkImage)
+                self._canvas.create_image(i*100+50, j*100+50, image = tkImage)
+                """
         self._canvas.configure(scrollregion=self._canvas.bbox('all'))
 
 cells = []
 for i in range(3):
     for j in range(3):
-        cells.append(Cell(i, j), "Assets/stone2.jpg")
+        cells.append(Cell(i, j, "Assets/stone2.jpg"))
 
 app = Application(cells=cells)
 app.master.title("test")
